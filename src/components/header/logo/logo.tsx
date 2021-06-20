93f8c4468ce2b78d419e9bf906eb848860a64867
uRaczy <raczy7@gmail.com>
2021-06-21 00:10:20 +0200

    Initial commit

---
+++ src/components/header/logo/logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = () => {
+  return (
+    <div>
+      <object className='logo' data="./../../assets/logo.svg" type="img/svg+xml"></object>
+    </div>
+  );
+}
+
+export default Logo;
