4d59e870367af6e14b2b1b7c88d44821496c1dac
uRaczy <raczy7@gmail.com>
2021-06-22 17:45:41 +0200

    Add hook to handle value of searchfield

--- src/components/header/searchbar/searchbar.component.tsx
+++ src/components/header/searchbar/searchbar.component.tsx
@@ -1,14 +1,17 @@
-import React, {useState} from 'react';
+import React, {SyntheticEvent, useState} from 'react';
 
 import './searchbar.css';
 
 const Searchbar : React.FC = () => {
   const [ searchValue, setSearchValue ] = useState('');
+  const handleChange = (e:any) => {
+    setSearchValue(e.target.value);
+  }
 
   return (
     // Forms in react are diffrent, info: https://reactjs.org/docs/forms.html
     <div className='searchBar'>
-      <input className='searchField' type='search' placeholder="Search.."></input>
+      <input className='searchField' type='search' value={searchValue} onChange={handleChange} placeholder="Search.."></input>
       <div className='searchBtn'>
         <span className="material-icons">
           search
